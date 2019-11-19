4e432f6e06c2421db9fd50aee9077cbcbfca742e
Murodjon <tmurodon@gmail.com>
2019-11-19 15:01:01 +0100

    Updates

---
+++ precache-manifest.0824783887226f196c9d8b84b787fb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe3408c9451b7cd95597a6556d85c20",
+    "url": "/FinalTestGP/index.html"
+  },
+  {
+    "revision": "d355de67aba6f06a15c0",
+    "url": "/FinalTestGP/static/css/main.1cd094b2.chunk.css"
+  },
+  {
+    "revision": "a8d5a8dcb6a1095c3a2a",
+    "url": "/FinalTestGP/static/js/2.f421bf73.chunk.js"
+  },
+  {
+    "revision": "d355de67aba6f06a15c0",
+    "url": "/FinalTestGP/static/js/main.a66d18fd.chunk.js"
+  },
+  {
+    "revision": "84995a367c5d15e2b8bd",
+    "url": "/FinalTestGP/static/js/runtime-main.b973546f.js"
+  }
+]);
